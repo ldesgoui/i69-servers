bf03f459ee700a2ad9a562a49613161c7d84756c
ldesgoui <ldesgoui@gmail.com>
2022-07-28 17:18:04 +0200

    add initialPassword for root

--- proxmox-images.nix
+++ proxmox-images.nix
@@ -33,6 +33,7 @@
 
     services.openssh = {
       enable = true;
+      passwordAuthentication = false;
     };
 
     networking.firewall = {
@@ -40,6 +41,7 @@
     };
 
     users.users.root = {
+      initialPassword = "toor";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK25ea20daUVvmTPmUL1nF/0DXEz/7tPBXOSerQNTf6+ me@ldesgoui.xyz"
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC94Qd2XQoffOcrsEbZfwOzNPy8PZgyAchycchIohhYVnqx5EVwlBMbOBKR5VUnXXxiglFS+HvfAUmHJth5dBdnfuUr3f1wkth5w+KwjnWzIlrFW30+zsc1CsUfluUQc1kW2qx8Z/ytS28zgL/B14yEcRoTgMJBjmWkLe060lROx1VR8Elp3sRJjClTZ64o6CeM4QaxIaDd1ZL008KcAEcK1bcScpvCSfRGCUAu+TTwxz0/cyb9x21I9x67FpELlAegw3y/6A7tbpSFZY/WG+eRwrTeAXK799rzV6Brg5RadOVa3PAsxOpIjgLQ/klH+m487UNCuoNE+SiukoP3OUjh proto"
